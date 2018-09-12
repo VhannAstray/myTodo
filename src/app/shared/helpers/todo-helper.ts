52a0ddd654eae66d40598ad7a748ecfce5c069d1
Vincent <vincent.dovidio@gmail.com>
2018-09-12 16:08:30 +0200

    Création du mat-table-helper

--- src/app/shared/helpers/todo-helper.ts
+++ src/app/shared/helpers/todo-helper.ts
@@ -1,9 +1,10 @@
-import { MatColumns } from './../interfaces/mat-columns';
+import { MatTableHelper } from './mat-table-helper';
 
-export class TodoHelper {
-    private todoTableMap: Map<String, MatColumns> = new Map();
+export class TodoHelper extends MatTableHelper{
 
     public constructor() {
+        super(); //Instance la classe parent
+
         this.todoTableMap.set(
             'title',
             { title: 'A faire', always: true, value: 'title', isDisplayed: true }
@@ -24,83 +25,5 @@ export class TodoHelper {
             'delete',
             { title: '', always: true, value: 'delete', isDisplayed: true }
         );
-    }
-
-    /**
-     * Méthode qui renvoi la liste des colonnes à afficher
-     */
-    public getDisplayedColumns(): String[] {
-        const toDisplay: String[] = [];
-
-        this.todoTableMap.forEach((column) => {
-            if (column.isDisplayed) {
-                toDisplay.push(column.value);
-            }
-        });
-        return toDisplay;
-    }
-
-    /**
-     * Renvoi la colonne passé en 'key'
-     * @param key Paramètre correspondant à la colonne
-     */
-    public getColumn(key: String): MatColumns {
-        return this.todoTableMap.get(key);
-    }
-
-    /**
-     * Retourne les colonnes à afficher dans la liste à sélectionné
-     */
-    public getOptionalColumns(): MatColumns[] {
-        const toDisplayList: MatColumns[] = [];
-        this.todoTableMap.forEach((column) => {
-            if (!column.always) {
-                toDisplayList.push(column);
-            }
-        });
-        return toDisplayList;
-    }
-
-    /**
-     * Méthode qui change la visibilité d'un élément passer en paramètre
-     * @param key Chaîne de caractères représentant la colonne
-     */
-    public toggleVisibility(key: String) {
-        let colonneTemp: MatColumns = this.todoTableMap.get(key);
-        colonneTemp.isDisplayed = !colonneTemp.isDisplayed;
-        this.todoTableMap.set(key, colonneTemp);
-    }
-
-    /** Méthode de Jean luc */
-    /**
-     * Retourne la liste des colonnes à afficher
-     * @param userSelection userSelection Tableau contenant la sélection utilisateur
-     */
-    public setDisplayedColumns(userSelection: String[]): String[] {
-        this.todoTableMap.forEach((column, key) => {
-            if (!column.always) {
-                if (userSelection.indexOf(column.value) === -1) {
-                    column.isDisplayed = false;
-                } else {
-                    column.isDisplayed = true;
-                }
-                this.todoTableMap.set(key, column);
-            }
-        });
-        return this.getDisplayedColumns();
-    }
-
-    /**
-     * Retourne le tableau des colonnes optionnelles
-     */
-    public optionalColumnsToArray(): String[] {
-        const toDisplay: String[] = [];
-
-        this.todoTableMap.forEach((column, key) => {
-            if (!column.always) {
-                toDisplay.push(column.value);
-            }
-        });
-        return toDisplay;
     }
 }
